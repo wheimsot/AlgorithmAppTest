34d77d7130c247a4142bdfc93b2844fdba9a5ceb
wheimsot <wheim5@yahoo.com>
2016-10-20 13:20:10 -0400

    Added beat times text box, ui_convert.py and added main_window.py to .gitignore

---
+++ run.py
@@ -0,0 +1,8 @@
+import
+
+
+class ExampleApp(QtGui.QMainWindow, ui_main.Ui_MainWindow):
+    def __init__(self, parent=None):
+        pyqtgraph.setConfigOption('background', 'w') #before loading widget
+        super(ExampleApp, self).__init__(parent)
+        self.setupUi(self)
