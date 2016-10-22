d4c805815025645dd9e688f0b726da39c993ef37
wheimsot <wheim5@yahoo.com>
2016-10-22 16:11:19 -0400

    Gets file names

--- run.py
+++ run.py
@@ -15,14 +15,46 @@ class MainWindow(QtGui.QMainWindow, main_window.Ui_MainWindow):
         self.inputPlot.plotItem.showGrid(True, True, 0.7)
         self.filePlot.plotItem.showGrid(True, True, 0.7)
 
+
+        self.alg = Algorithm()
         self.inFile = ''
+        self.run = False
+
+    @QtCore.pyqtSignature("")
+    def on_fileButton_clicked(self):
+        dlg = QtGui.QFileDialog()
+        dlg.setFileMode(QtGui.QFileDialog.AnyFile)
+        dlg.setFilter("Wave files (*.wav)")
+        #filenames = QStringList()
+        if dlg.exec_():
+            filenames = dlg.selectedFiles()
+            print(filenames)
 
     @QtCore.pyqtSignature("")
     def on_startButton_clicked(self):
-        print('hello')
+        self.run = True
+        self.alg.start()
+        #thread = Algorithm()
+        #thread.run()
+
+    @QtCore.pyqtSignature("")
+    def on_stopButton_clicked(self):
+        print('Stopping')
+        self.run = False
+        self.alg.terminate()
 
     def closeEvent(self, *args, **kwargs):
         print('implement this')
+
+
+class Algorithm(QtCore.QThread):
+    def __init__(self):
+        QtCore.QThread.__init__(self)
+
+    def run(self):
+        while self.run:
+            print('implement')
+            time.sleep(1)
 
 
 if __name__ == "__main__":
