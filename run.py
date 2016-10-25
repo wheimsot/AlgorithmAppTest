f0bdb3f09dc5603e2b64dd1162c304740e3384c9
wheimsot <wheim5@yahoo.com>
2016-10-24 22:33:08 -0400

    Most backend functionality working. TODO: add algorithm and pyaudio

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from PyQt4 import QtGui,QtCore
 import sys
 import main_window
 import numpy as np
+import scipy.io.wavfile
 import pyqtgraph
 import time
 
@@ -11,10 +12,13 @@ class MainWindow(QtGui.QMainWindow, main_window.Ui_MainWindow):
         pyqtgraph.setConfigOption('background', 'w') #before loading widget
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
+        # Enable/disable required buttons
+        self.startButton.setEnabled(False)
+        self.stopButton.setEnabled(False)
+        self.fileButton.setEnabled(True)
 
         self.inputPlot.plotItem.showGrid(True, True, 0.7)
         self.filePlot.plotItem.showGrid(True, True, 0.7)
-
 
         self.alg = Algorithm()
         self.inFile = ''
@@ -25,26 +29,43 @@ class MainWindow(QtGui.QMainWindow, main_window.Ui_MainWindow):
         dlg = QtGui.QFileDialog()
         dlg.setFileMode(QtGui.QFileDialog.AnyFile)
         dlg.setFilter("Wave files (*.wav)")
-        #filenames = QStringList()
         if dlg.exec_():
-            filenames = dlg.selectedFiles()
-            print(filenames)
+            wav_path = dlg.selectedFiles()[0]
+            beats_path = wav_path.replace('.wav', '.txt')
+            lines = open(beats_path).read().splitlines()
+            self.beat_times = np.asarray(lines, dtype=np.float)
+            self.rate, self.wav_data = scipy.io.wavfile.read(wav_path)
+            pen = pyqtgraph.mkPen(color='b')
+            t = np.arange(0, self.wav_data.size/self.rate, step=1/self.rate)
+            self.filePlot.plot(t, self.wav_data.astype(np.float), pen=pen, clear=True)
+            self.filePlot.plot(self.beat_times, np.zeros_like(self.beat_times), pen=None, symbol='o', clear=False)
+            self.startButton.setEnabled(True)
+            self.stopButton.setEnabled(False)
+            self.fileButton.setEnabled(True)
+
 
     @QtCore.pyqtSignature("")
     def on_startButton_clicked(self):
+        print('Starting...')
         self.run = True
         self.alg.start()
+        self.startButton.setEnabled(False)
+        self.stopButton.setEnabled(True)
+        self.fileButton.setEnabled(False)
         #thread = Algorithm()
         #thread.run()
 
     @QtCore.pyqtSignature("")
     def on_stopButton_clicked(self):
-        print('Stopping')
+        print('Stopping...')
         self.run = False
         self.alg.terminate()
+        self.startButton.setEnabled(True)
+        self.stopButton.setEnabled(False)
+        self.fileButton.setEnabled(True)
 
     def closeEvent(self, *args, **kwargs):
-        print('implement this')
+        print('Closing...')
 
 
 class Algorithm(QtCore.QThread):
